f3847b1cb38fa2a0e792624a0c16e827656ae014
LitanMolla <litanmolla9@gmail.com>
2025-10-22 18:34:54 +0600

    added: mobile responsive hambergun bar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const darklight = document.getElementById('darklight');
+const menuBar = document.getElementById('menuBar');
+const mobileMenu = document.getElementById('mobileMenu');
 const html = document.getElementsByTagName('html')[0];
-darklight.addEventListener('click',()=>{
+
+darklight.addEventListener('click', () => {
     html.classList.toggle('dark');
-    console.log(html.classList);
-})
+});
+
+menuBar.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
